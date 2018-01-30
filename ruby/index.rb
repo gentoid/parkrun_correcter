003e7aea6a84033eda615fc32d76d3324ac1b7a0
Ilya Lazarev <crusadergo@yandex.ru>
2018-01-31 01:32:47 +0500

    Add puts, gets and split for requesting invalid positions

---
+++ ruby/index.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+input_filename = ARGV # Array
+
+puts "Please, enter the invalid positions:"
+STDOUT.flush
+invalid_positions = STDIN.gets.chomp
+invalid_positions.split # Array
+  
