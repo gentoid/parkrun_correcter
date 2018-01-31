a947225edfd7cab5ea620827a3e6ada08e959c0f
Ilya Lazarev <crusadergo@yandex.ru>
2018-01-31 18:49:20 +0500

    Add simple script implementation

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,9 +1,24 @@
 #!/usr/bin/env ruby
 
-input_filename = ARGV # Array
+app_root = File.expand_path File.dirname("../")
+input_filename = ARGV[0]
+filename_without_extension = input_filename.split('.').first
+corrected_file =  File.new(File.join(app_root, "#{ filename_without_extension }_corrected.txt"), 'w')
 
-puts "Please, enter the invalid positions:"
+p 'Please, enter space-separated invalid positions:'
 STDOUT.flush
 invalid_positions = STDIN.gets.chomp
-invalid_positions.split # Array
-  
+
+p 'Removing invalid positions...'
+
+File.open(File.join app_root, input_filename) do |source|
+  source.each_line do |line|
+    position = line.split(',').first
+
+    unless invalid_positions.split.include? position
+      corrected_file << line
+    end
+  end
+end
+
+p 'Removed positions: ' + invalid_positions
